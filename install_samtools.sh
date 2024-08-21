8bf379ae31aef84da69d8b90b8b17891095310ab
Ian Vermes <iv3@sanger.ac.uk>
2024-08-21 21:33:49 +0000

    Add CLI option for run_tests.sh to use pre-compiled htslib

--- install_samtools.sh
+++ install_samtools.sh
@@ -9,7 +9,8 @@ SETUP_DIR=""
 INSTALL_DIR=""
 PROGRAM_VERSION=""
 IS_DEFAULT_INSTALL_DIR=1 # 1 == true, 0 == false
-
+IS_HTSLIB_BUILD_DIR_SET=0 # 1 == true, 0 == false
+HTSLIB_BUILD_DIR="na"
 
 ### CONSTANTS ###
 PROGRAM_NAME="samtools"
@@ -40,9 +41,9 @@ function print_error() {
 }
 
 function print_usage() {
-  echo "Usage: $0  <SAMTOOLS-VERSION> [--install-dir <INSTALL-DIR>] [--setup-dir <SETUP-DIR>] [-h|--help] [--version]"
+  echo "Usage: $0  <SAMTOOLS-VERSION> [--install-dir <INSTALL-DIR>] [--setup-dir <SETUP-DIR>] [ --installed-htslib-dir <HTSLIB-DIR>] [-h|--help] [--version]"
   echo ""
-  echo "Installs ${PROGRAM_NAME:?}, compiling it from source."
+  echo "Installs ${PROGRAM_NAME:?} only, compiling it from source. This script does not install htslib."
   echo ""
   echo "Arguments:"
   echo "  SAMTOOLS-VERSION: The version of ${PROGRAM_NAME:?} to install e.g. 1.14"
@@ -52,6 +53,9 @@ function print_usage() {
   echo "                    ${PROGRAM_NAME:?} into, must be an absolute path and writable e.g. /opt/${PROGRAM_NAME:?}"
   echo "  --setup-dir:      [Default: (set by mktemp)] The directory to download and unpack "
   echo "                    the ${PROGRAM_NAME:?} tarball (must be writable). Deleted after installation."
+  echo "  --installed-htslib-dir:   If specified, the path to an existing htslib installation is used to "
+  echo "                    compile samtools. Otherwise, samtools will compile with the bundled "
+  echo "                    htslib source (this WILL NOT install htslib)."
   echo "  -h, --help:       Print this message"
   echo "  --version:        Print the version of this script"
   echo ""
@@ -70,6 +74,7 @@ function parse_args() {
   local user_install_dir=""
   local program_version=""
   local is_default_install_dir=""
+  local htslib_dir=""
 
   # Parse the command-line arguments
   while [ "$#" -gt 0 ]; do
@@ -98,6 +103,15 @@ function parse_args() {
           exit 1
         fi
         user_install_dir="${2}"
+        shift
+        ;;
+      --installed-htslib-dir)
+        if [ -z "${2}" ]; then
+          print_error "Missing argument for --installed-htslib-dir"
+          print_usage
+          exit 1
+        fi
+        htslib_dir="${2}"
         shift
         ;;
       *)
@@ -180,6 +194,13 @@ function parse_args() {
     print_warning "Unexpected ${PROGRAM_NAME:?} version: ${program_version}, expected format is X.Y"
   fi
 
+  # `htslib_dir`
+  # Check that the htslib directory is valid: it is a directory
+  if [[ -n "${htslib_dir}" ]] && [[ ! -d "${htslib_dir}" ]]; then
+    print_error "Invalid htslib directory: ${htslib_dir}"
+    exit 1
+  fi
+
   # Set the global variables
   INSTALL_DIR="${install_dir}"
   print_info "Setting INSTALL_DIR=${INSTALL_DIR}"
@@ -189,6 +210,12 @@ function parse_args() {
   print_info "Setting PROGRAM_VERSION=${PROGRAM_VERSION}"
   IS_DEFAULT_INSTALL_DIR="${is_default_install_dir}"
   print_info "Setting IS_DEFAULT_INSTALL_DIR=${IS_DEFAULT_INSTALL_DIR} (1 == true, 0 == false)"
+  if [[ -n "${htslib_dir}" ]]; then
+    IS_HTSLIB_BUILD_DIR_SET=1
+    HTSLIB_BUILD_DIR="${htslib_dir}"
+    print_info "Setting HTSLIB_BUILD_DIR=${HTSLIB_BUILD_DIR}"
+  fi
+
 }
 
 function assert_programs_exists() {
@@ -306,15 +333,19 @@ function get_cpu_count() {
 }
 
 function install() {
+  # Args
   local install_dir="${1}"
   local work_dir="${2}"
   local is_default_install_dir="${3}"
-
+  local htslib_build_dir="${4}" # This value could be "na" if not set -- rely on is_htslib_build_dir_set to determine if it's set
+  local is_htslib_build_dir_set="${5}"  # 1 == true, 0 == false
+  
+  # Local variables
   local cmd=""
-  local cmd_required_args=" "
   local cmd_non_default_arg__prefix="--prefix=${install_dir:?}"
   local cmd_non_default_arg__cppflags="CPPFLAGS=\"-I${install_dir:?}/include\""
   local cmd_non_default_arg__ldflags="LDFLAGS=\"-L${install_dir:?}/lib -Wl,-R${install_dir:?}/lib\""
+  local cmd_optional_arg__htslib="--with-htslib=${htslib_build_dir:?}"
   local cpu_count=$(get_cpu_count 6)
 
 
@@ -336,14 +367,13 @@ function install() {
 
   print_info "Running configure script"
   cmd="./configure"
-  if [ "${is_default_install_dir:?}" -eq 1 ]; then
-    cmd="${cmd:?} ${cmd_required_args:?}"
-  else
-    # Add non-default arguments (concatenated with spaces)
-    cmd="${cmd:?} ${cmd_required_args:?}"
+  if [ "${is_default_install_dir:?}" -eq 0 ]; then
     cmd="${cmd:?} ${cmd_non_default_arg__prefix:?}"
     cmd="${cmd:?} ${cmd_non_default_arg__cppflags:?}"
     cmd="${cmd:?} ${cmd_non_default_arg__ldflags:?}"
+  fi
+  if [ "${is_htslib_build_dir_set:?}" -eq 1 ]; then
+    cmd="${cmd:?} ${cmd_optional_arg__htslib:?}"
   fi
 
   print_info "Running configure with command: '${cmd:?}'"
@@ -370,6 +400,8 @@ function main() {
   local setup_dir="${2}"
   local program_version="${3}"
   local is_default_install_dir="${4}"
+  local htslib_build_dir="${5}"
+  local is_htslib_build_dir_set="${6}"
 
   # Local constants
   local tarball="${install_dir:?}/${PROGRAM_NAME:?}-${program_version:?}.${TARBALL_SUFFIX:?}"
@@ -394,7 +426,7 @@ function main() {
   export MANPATH=$(clean_path_strings "${MANPATH:?}")
 
   # Run the installation
-  install "${install_dir:?}" "${unpack_dir:?}" "${is_default_install_dir:?}"
+  install "${install_dir:?}" "${unpack_dir:?}" "${is_default_install_dir:?}" "${htslib_build_dir:?}" "${is_htslib_build_dir_set:?}"
 
   # Clean up the installation directory
   clean_up "${setup_dir:?}" "${tarball:?}"
@@ -409,6 +441,6 @@ assert_programs_exists "${REQUIRED_PROGRAMS[@]}"
 # Save the current working directory and restore it when the script exits
 cwd=$(pwd)
 trap 'exit_code=$?; cd "$cwd"; exit $exit_code' EXIT SIGHUP SIGINT SIGTERM SIGQUIT
-main "${INSTALL_DIR:?}" "${SETUP_DIR:?}" "${PROGRAM_VERSION:?}" "${IS_DEFAULT_INSTALL_DIR:?}"
+main "${INSTALL_DIR:?}" "${SETUP_DIR:?}" "${PROGRAM_VERSION:?}" "${IS_DEFAULT_INSTALL_DIR:?}" "${HTSLIB_BUILD_DIR:?}" "${IS_HTSLIB_BUILD_DIR_SET:?}"
 
 # End of script
